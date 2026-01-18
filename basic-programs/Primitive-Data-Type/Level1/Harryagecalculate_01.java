c222aa7400abe21d6c2b02b9dd11a90322802b40
tarrun01 <tarun.ttsindia@gmail.com>
2026-01-18 22:18:48 +0530

    Lvl 1 Problem Solutions

--- basic-programs/Primitive-Data-Type/Level1/Harryagecalculate_01.java
+++ basic-programs/Primitive-Data-Type/Level1/Harryagecalculate_01.java
@@ -1,6 +1,6 @@
 //Author-Tarun Kumar Sec-AT Program-Btech CSE
 //Date-18/01/2026; Last Edited Date-18/01/2026
-//Purpose-Write a program to find the age of Harry if the birth year is 2000.Assume the Current Year is 2024
+//Purpose-Write a program to find the age of Harry if the birth year is 2000.Assume the Current Year is 2024 
 public class Harryagecalculate_01 {
     public static void main(String args[]){
         int dobharry=2000;
