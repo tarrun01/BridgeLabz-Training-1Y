db1af10644228eb1169550b1059587f0b9727be4
tarrun01 <tarun.ttsindia@gmail.com>
2026-01-18 22:27:54 +0530

    Lvl 1 Problem Solutions

--- basic-programs/Primitive-Data-Type/Level1/kmtomiles_03.java
+++ basic-programs/Primitive-Data-Type/Level1/kmtomiles_03.java
@@ -1,6 +1,6 @@
 //Author-Tarun Kumar Sec-AT Program-Btech CSE
 //Date-18/01/2026; Last Edited Date-18/01/2026
-//Purpose-Create a program to convert the distance of 10.8 kilometers to miles.
+//Purpose-Create a program to convert the distance of 10.8 kilometers to miles;
 public class kmtomiles_03 {
     public static void main(String[] args) {
        double distancekm=10.8;  
