eaf5f2b368913cd90e42567cf7b2ac307358e986
Tarun Kumar <tarun.ttsindia@gmail.com>
2026-01-21 00:26:00 +0530

    Update Students.java

--- basic-programs/Practice-Programs/Students.java
+++ basic-programs/Practice-Programs/Students.java
@@ -4,15 +4,15 @@ class Students {
    String name;
    void printdetails(){
       String college ="GLA University";
-      System.out.println("ID is :"+id);
-      System.out.println("Name is :"+name);
-      System.out.println("Collage is :"+college);
+      System.out.println("ID :"+id);
+      System.out.println("Name :"+name);
+      System.out.println("Collage :"+college);
    }
    public static void main(String[] args) {
       Students s1=new Students();
       s1.id=101;
       s1.age=18;
-      s1.name="Tarun";
+      s1.name="Tarun Kumar";
       s1.printdetails();
    }
    }
